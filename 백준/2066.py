c8d54806cedc68a55b8d55ec1e4a214498b05bc4
zzimnii <102315673+zzimnii@users.noreply.github.com>
2024-11-19 13:37:31 +0900

    [Silver 3] Title: 바이러스, Times: 56ms, Memory: 34052KB

--- 백준/2066.py
+++ 백준/2066.py
@@ -2,34 +2,6 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-# def BFS(x):
-#     count = 0
-#     visited[x]=True
-#     q = deque([x])
-
-#     while(q):
-#         n = q.popleft()
-#         for next in graph[n]:
-#             if not visited[next]:
-#                 visited[next] = True
-#                 q.append(next)
-#                 count += 1
-
-#     return count
-
-# computers = int(input())
-# n = int(input())
-
-# graph = [[] for _ in range(computers+1)]
-# for _ in range(n):
-#     a, b = map(int, input().split())
-#     graph[a].append(b)
-#     graph[b].append(a)
-    
-# visited = [False for _ in range(computers+1)]
-# print(BFS(1))
-
-
 computers = int(input())
 n = int(input())
 graph = [[] for _ in range(computers+1)]
